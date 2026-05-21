fb505aaf93d72ee90d426227b0b34ba61f686295
Alluisio Silva <alluisio.silva@ipdec.org>
2026-05-21 13:09:57 -0400

    feat: implementação das novas constantes de cores

---
+++ src/Constants/Colors.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+  firstLinerGradient: "#1DDBB9",
+  backgroundPrimary: "#132742",
+  itemMenuAtivo: "#2CCBB9",
+  backgroundButton: "#14384D",
+  attentionTextColor: "#1CCBAE",
+  commonTextColor: "#fff",
+  contrastTextColor: "#000",
+  inactiveTextColor: "#656363",
+  dangerActionColor: "#FF5555",
+};
